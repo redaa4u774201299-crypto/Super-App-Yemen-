6197ded9cdd3fb385ad8c6424bb18416bac33602
redaa4u774201299-crypto <redaa4u774201299@gmail.com>
2026-07-28 23:58:58 +0300

    Create Wallet model with userId and balance fields

---
+++ wallet_model.dart
@@ -0,0 +1,17 @@
+class Wallet {
+
+final String userId;
+final double balance;
+final double frozenBalance;
+
+
+Wallet({
+
+required this.userId,
+required this.balance,
+required this.frozenBalance,
+
+});
+
+}
+
