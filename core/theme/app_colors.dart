d2ba1674b9b18719360277bb0fa966b56fd8e36d
redaa4u774201299-crypto <redaa4u774201299@gmail.com>
2026-07-28 22:54:09 +0300

    Create app_colors.dart

---
+++ core/theme/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+ static const navy = Color(0xff071A33);
+
+ static const maroon = Color(0xff7A1F35);
+
+ static const gold = Color(0xffD4AF37);
+
+ static const beige = Color(0xffF5EFE6);
+
+ static const white = Colors.white;
+
+ static const text = Color(0xff222222);
+
+}
