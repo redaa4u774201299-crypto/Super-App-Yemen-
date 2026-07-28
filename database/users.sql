932723ac66467e95f214b5321c8d8791204d6d21
redaa4u774201299-crypto <redaa4u774201299@gmail.com>
2026-07-28 23:14:28 +0300

    Create users table in database

---
+++ database/users.sql
@@ -0,0 +1,17 @@
+EnterCREATE TABLE users (
+
+id INTEGER PRIMARY KEY,
+
+name VARCHAR(100),
+
+phone VARCHAR(20),
+
+avatar TEXT,
+
+city VARCHAR(50),
+
+wallet_balance DECIMAL,
+
+created_at TIMESTAMP
+
+);
