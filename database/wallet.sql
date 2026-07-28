9165fc5234984ba088f75a84074790e3d45d0cf2
redaa4u774201299-crypto <redaa4u774201299@gmail.com>
2026-07-29 00:00:26 +0300

    Create wallets table in database

---
+++ database/wallet.sql
@@ -0,0 +1,16 @@
+CREATE TABLE wallets (
+
+id INTEGER PRIMARY KEY,
+
+user_id INTEGER,
+
+balance DECIMAL DEFAULT 0,
+
+frozen_balance DECIMAL DEFAULT 0,
+
+currency VARCHAR(10)
+DEFAULT 'YER',
+
+created_at TIMESTAMP
+
+);
